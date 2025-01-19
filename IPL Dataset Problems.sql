adf6ddcd434611a69b36ec597933fdc367196088
G1Codes <jeevan983+github@gmail.com>
2025-01-20 01:02:25 +0530

    Solved More Questins

--- IPL Dataset Problems.sql
+++ IPL Dataset Problems.sql
@@ -8,3 +8,44 @@ group by battingTeam, batter) t
 where rank_in_team<6
 order by battingteam, rank_in_team
 ;
+
+-- Q. Virat’s Total Runs after 50th, 100th & 200th match
+
+SELECT * FROM
+(SELECT 
+CONCAT ("Match-",
+ROW_NUMBER() OVER(ORDER BY ID))as 'Match',
+SUM(BATSMAN_RUN) AS Runs,
+SUM(SUM(BATSMAN_RUN)) OVER (ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS Cumulative_Runs
+from ipl
+WHERE batter = "V kohli"
+group by ID) t
+where t.Match = 'Match-50' OR t.Match = 'Match-100' OR t.Match = 'Match-200'
+  ;
+
+-- Q. Find out Kohli’s average after each match.
+
+SELECT * FROM
+(SELECT 
+CONCAT ("Match-",
+ROW_NUMBER() OVER(ORDER BY ID))as 'Match',
+SUM(BATSMAN_RUN) AS Runs,
+AVG(SUM(BATSMAN_RUN)) OVER (ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS Career_Avg
+from ipl
+WHERE batter = "V kohli"
+group by ID) t
+;
+
+-- Q. Calculate Virat’s running average over 10 matches.
+
+SELECT * FROM
+(SELECT 
+CONCAT ("Match-",
+ROW_NUMBER() OVER(ORDER BY ID))as 'Match',
+SUM(BATSMAN_RUN) AS Runs,
+AVG(SUM(BATSMAN_RUN)) OVER (ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS Cumulative_Runs,
+AVG(SUM(BATSMAN_RUN)) OVER (ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS Running_Avg
+from ipl
+WHERE batter = "V kohli"
+group by ID) t
+;
